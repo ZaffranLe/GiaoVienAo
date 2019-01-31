af80c319206fe1907c385607678149c45b5fc03f
Zaffran <biprono12@gmail.com>
2019-01-31 12:26:35 +0700

    Sửa tiêu chí xếp loại

--- src/views/Survey/Survey.js
+++ src/views/Survey/Survey.js
@@ -121,31 +121,33 @@ class Survey extends Component {
 
   calculateResult(resultList){
     var i;
+    var dat = 0;
     var kha = 0;
     var tot = 0;
     for (i = 0; i < resultList.length; i++){
       // eslint-disable-next-line
       if (resultList[i] == 1) return "Chưa đạt";
       // eslint-disable-next-line
-      resultList[i] == 3 ? kha++ : resultList[i] == 4 && tot++; // Short circuiting
+      resultList[i] == 3 ? kha++ : resultList[i] == 4 ? tot++ : dat++;
     }
 
-    if (kha >= 10){
-      for (i = 2; i < 7; i++){
-        // eslint-disable-next-line
-        if (resultList[i] == 2) return "Đạt";
-      }
-      return "Khá";
+    for (i = 2; i < 7; i++) {
+      // eslint-disable-next-line
+      if (resultList[i] == 2) return "Đạt";
     }
+
+    if (dat >= 6) return "Đạt";
+
+
     if (tot >= 10) {
       // eslint-disable-next-line
       for (i = 2; i < 7; i++) {
         // eslint-disable-next-line
-        if ((resultList[i] == 3) || (resultList[i] == 2)) return "Khá";
+        if (resultList[i] == 3) return "Khá";
       }
       return "Tốt";
     }
-    return "Đạt";
+    return "Khá";
   }
 
   render() {
